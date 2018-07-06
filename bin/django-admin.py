cf840b9f69b14d5c193f0b6f6b5a127ea73b42c3
gersoncalete <fabiocalete@gmail.com>
2018-07-06 02:16:00 +0200

    CSS

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fabio/Documents/software/Python/environments/angularjs-assignment/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
